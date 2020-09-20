9b4ed0c0189adf22b3163277be44a125c00b8e53
Sakshet Chavan <sakshet@shoptimize.in>
2020-09-20 11:02:11 +0530

    Accepts file from user on cmd line and opens the file in Read mode.

--- assignments/cmd-file-accept-open/code.c
+++ assignments/cmd-file-accept-open/code.c
@@ -1,11 +1,12 @@
-
-
 #include<stdio.h>
 #include<fcntl.h>
 #include "../../tlpi-dist/lib/tlpi_hdr.h"
 
 int main(int argc, char* argv[]){
-
+	// char arr[11];
+	// char brr[4];
+	// char crr[4];
+	// int ret;
 	if(argc == 1 || argc > 2 || strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"-help") == 0 || strcmp(argv[1],"-H") == 0 ){
 		usageErr("%s <file-to-open> \n" , argv[0]);
 	}
@@ -15,7 +16,16 @@ int main(int argc, char* argv[]){
 		errExit("Error opening file %s\n", argv[1]);
 	}
 
-	printf("\"%s\" file opened with FD = %d\n", argv[1], fd);
+	printf("\"%s\" file opened with FD = %d\t\n", argv[1], fd);
+
+	// ret = read(fd, arr, 10);
+	// printf("%s\n", arr);
+
+	// ret = read(fd, brr, 4);
+	// printf("%s\n", brr);
+
+	// ret = read(fd, crr, 4);
+	// printf("%s\n", crr);
 
 	return 0;
 }
