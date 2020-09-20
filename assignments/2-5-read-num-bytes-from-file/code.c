a2a7c40064e6a0d284354a0ff785b6e20a12820a
Sakshet Chavan <sakshet@shoptimize.in>
2020-09-20 11:39:00 +0530

    Cleaned code.

--- assignments/2-5-read-num-bytes-from-file/code.c
+++ assignments/2-5-read-num-bytes-from-file/code.c
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
 	int fd = open(argv[1], O_RDONLY);
 	
 	if (fd == -1){
-		errExit("Error opening file %s\n", argv[1]);
+		errExit(": \tError opening file %s\n", argv[1]);
 	}
 
 	ret = read(fd, rdbuf, buf);
